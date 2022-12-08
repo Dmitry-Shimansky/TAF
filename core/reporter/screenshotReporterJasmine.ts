3770b3075e5ec291a41ce08ed570d428182397d0
Dmitry Shimansky <dshimansky@foundationmedicine.com>
2022-12-09 00:04:37 +0300

    additions

--- core/reporter/screenshotReporterJasmine.ts
+++ core/reporter/screenshotReporterJasmine.ts
@@ -7,10 +7,10 @@ class ScreenshotReporter {
     screenshotMaker = {
         specDone : async function(result) {
             const screenshotName = new Date().getTime() + '.png';
-            const path = `./testResultsScreenshot/`;
+            const path = `./testResultsScreenshot/${screenshotName}`;
             if (result.status === 'failed') {
                 logger.info(`${result} \n Screenshot taken at ${new Date().toString()} for current step is ${screenshotName},
-            folder: /${path}/`);
+            folder: /${path}/${screenshotName}`);
                 await global.page.screenshot({ path: path, fullPage: true, type: 'png' });
             }
         },
